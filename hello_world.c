7546f52551879c19b5c93ba2c2ccac1d129f0488
Gyanesh <gkm010@gmail.com>
2012-10-01 12:12:33 +0530

    Update hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+/* this is a demo file with which every programmer starts with*/
+// hello world!!
+
+#include<stdio.h>
+//start of main function
+int main()
+{
+  printf("Hello-World!!");
+  return 0;
+}
+//end of the program
